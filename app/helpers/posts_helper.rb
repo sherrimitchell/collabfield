108b5da3d9a27740d10d7ca68fd3c6bbbadb5140
Sherri Mitchell <rsamitchell@att.net>
2018-04-23 19:05:58 -0400

    Add a search form in branch pages - Render a search form in the _branch.html.erb - Create a _search_form.html.erb partial - Define a category_field_partial_path helper method in PostsHelper - Create a _category_field.html.erb partial - Add css for the search form in branch_page.scss

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,5 @@
 module PostsHelper
 
-  def post_format_partial_path
-    current_page?(root_path) ? 'posts/post/home_page' : 'posts/post/branch_page'
-  end
-  
   def create_new_post_partial_path
     if user_signed_in?
       'posts/branch/create_new_post/signed_in'
@@ -22,6 +18,18 @@ module PostsHelper
 
   def no_posts_partial_path
     @posts.empty? ? 'posts/branch/no_posts' : 'shared/empty_partial'
+  end
+
+  def post_format_partial_path
+    current_page?(root_path) ? 'posts/post/home_page' : 'posts/post/branch_page'
+  end
+   
+  def category_field_partial_path
+    if params[:category].present?
+      'posts/branch/search_form/category_field'
+    else
+      'shared/empty_partial'
+    end
   end
 
 end
